852c53dfe8bc1b643af2fd6bca558b469c1acdd3
Shivangi Maurya <mauryashivangi007@gmail.com>
2026-06-29 10:43:42 +0530

    Initial commit

---
+++ data/user.js
@@ -0,0 +1,37 @@
+// Temporary data store
+// In future this will be replaced with MongoDB
+
+const users = [
+    {
+        id: 1,
+        name: "Shivangi",
+        email: "shivangi@gmail.com",
+        age: 20,
+    },
+    {
+        id: 2,
+        name: "Rahul",
+        email: "rahul@gmail.com",
+        age: 22,
+    },
+    {
+        id: 3,
+        name: "Priya",
+        email: "priya@gmail.com",
+        age: 21,
+    },
+    {
+        id: 4,
+        name: "Akshat",
+        email: "akshat@gmail.com",
+        age: 20,
+    },
+    {
+        id: 5,
+        name: "Aryan",
+        email: "aryan@gmail.com",
+        age: 21,
+    },
+];
+
+module.exports = users;
---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+// Create Express app
+const app = express();
+
+// Middleware
+
+// Allow requests from frontend/Postman
+app.use(cors());
+
+// Parse incoming JSON data
+app.use(express.json());
+
+// Temporary Home Route
+app.get("/", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "Welcome to DecodeLabs Backend API ",
+  });
+});
+
+// Server Port
+const PORT = process.env.PORT || 5000;
+
+// Start Server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
